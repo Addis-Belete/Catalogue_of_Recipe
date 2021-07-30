0dbcaed1ebd1fcc759844652b6b10674b9f3cd6c
Addis Belete <addis.bel@gmail.com>
2021-07-30 17:45:45 +0300

    Fetch data using asyn await

---
+++ src/Api/Recipe-data.js
@@ -0,0 +1,15 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
+const RecipeData = async () => {
+  try {
+    const response = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=2b280d469bf44b1280528a91ec861d2a&number=20', { mode: 'cors' });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.blob();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default RecipeData;
