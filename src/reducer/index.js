74fc993b3fc3dbd38b2a37bd086deeb772fedd38
Addis Belete <addis.bel@gmail.com>
2021-07-31 11:02:38 +0300

    Add recipeRerducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,3 +1,13 @@
+import { FILTER_RECIPE } from '../action';
 import RecipeData from '../Api/Recipe-data';
 
 const initialState = RecipeData();
+
+const recipeReducer = (state = initialState, action) => {
+  if (action.type === FILTER_RECIPE) {
+    return action.filter;
+  }
+  return state;
+};
+
+export default recipeReducer;
