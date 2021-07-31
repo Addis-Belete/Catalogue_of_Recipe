93ef4d74627c804bc41183d374da90ea0a87fc63
Addis Belete <addis.bel@gmail.com>
2021-07-31 16:25:20 +0300

    Add a function that call ajax call from server

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import { createStore } from 'redux';
+import axios from 'axios';
 import { FILTER_RECIPE } from '../action';
 import RecipeData from '../Api/Recipe-data';
 
+const RECIPES_LOAD = 'RECIPES_LOAD';
 const initialState = [];
-const recipeReducer = (state = Data()) => state;
-const store = createStore(recipeReducer);
-export default store;
+const recipeReducer = (state = initialState, action) => state;
+
+const fetchRecipes = async (dispatch, getState) => {
+  const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=2b280d469bf44b1280528a91ec861d2a&number=20');
+  dispatch({ type: RECIPES_LOAD, payload: response.results });
+};
+
+export { recipeReducer, fetchRecipes };
