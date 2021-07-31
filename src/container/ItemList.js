2f7c0b894157709de58d3ebdb6f0deff7d0b351c
Addis Belete <addis.bel@gmail.com>
2021-07-31 11:11:17 +0300

    Connect component to redux store

--- src/container/ItemList.js
+++ src/container/ItemList.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const ItemList = () => (
+const ItemList = ({ recipe }) => (
   <div>
     This component is responsible to display all recipe lists
   </div>
 
 );
+const mapStateToProps = (state) => ({
+  recipe: state.recipeReducer.results,
 
-export default ItemList;
+});
+
+export default connect(mapStateToProps)(ItemList);
