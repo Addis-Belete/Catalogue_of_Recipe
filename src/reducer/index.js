56f55323a389e21b1f25dfe3c068f5154cef7ab5
Addis Belete <addis.bel@gmail.com>
2021-07-31 16:18:24 +0300

    Install axios

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -4,12 +4,7 @@ import { createStore } from 'redux';
 import { FILTER_RECIPE } from '../action';
 import RecipeData from '../Api/Recipe-data';
 
-const Data = () => {
-  let initialState = [{ id: 1, title: 'food' }];
-  RecipeData().then((resp) => { initialState = resp.results; });
-  return initialState;
-};
-console.log(Data());
+const initialState = [];
 const recipeReducer = (state = Data()) => state;
 const store = createStore(recipeReducer);
 export default store;
