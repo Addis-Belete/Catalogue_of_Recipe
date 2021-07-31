091e1d9fdac1e966435290f760772c498f0c28ce
Addis Belete <addis.bel@gmail.com>
2021-07-31 18:02:53 +0300

    Add fetchRecipesFailure

--- src/action/index.js
+++ src/action/index.js
@@ -2,6 +2,7 @@ const FILTER_RECIPE = 'FILTER_RECIPE';
 const FETCH_RECIPE = 'FETCH_RECIPE';
 const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST';
 const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
+const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
 const recipeFilter = (filter) => ({
   type: FILTER_RECIPE,
   filter,
@@ -12,6 +13,13 @@ export const fetchRecipeRequests = () => ({
 
 export const fetchRecipeSuccess = (recipes) => ({
   type: FETCH_USER_SUCCESS,
+  payload: recipes,
+
+});
+
+export const fetchRecipesFailure = (error) => ({
+  type: FETCH_USER_FAILURE,
+  payload: error,
 
 });
 export { FILTER_RECIPE, recipeFilter, FETCH_RECIPE };
