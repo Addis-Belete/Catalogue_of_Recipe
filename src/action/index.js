530e9dc7b143a79a49e2176426f3a59a4781b92e
Addis Belete <addis.bel@gmail.com>
2021-07-31 18:11:37 +0300

    Add fetchRecipes action creator

--- src/action/index.js
+++ src/action/index.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
+
 const FILTER_RECIPE = 'FILTER_RECIPE';
-const FETCH_RECIPE = 'FETCH_RECIPE';
 const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST';
 const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
 const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
-const recipeFilter = (filter) => ({
+export const recipeFilter = (filter) => ({
   type: FILTER_RECIPE,
   filter,
 });
@@ -22,4 +23,16 @@ export const fetchRecipesFailure = (error) => ({
   payload: error,
 
 });
-export { FILTER_RECIPE, recipeFilter, FETCH_RECIPE };
+
+export const fetchRecipes = () => (dispatch) => {
+  dispatch(fetchRecipeRequests);
+  axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=2b280d469bf44b1280528a91ec861d2a&number=20')
+    .then((response) => {
+      const recipes = response.results;
+      dispatch(fetchRecipeSuccess(recipes));
+    })
+    .catch((error) => {
+      const errorMsg = error.message;
+      dispatch(fetchRecipesFailure(errorMsg));
+    });
+};
