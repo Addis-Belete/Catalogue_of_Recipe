101af6184bbac969c4bd61ab7e27346925eb2370
Addis Belete <addis.bel@gmail.com>
2021-08-01 18:50:19 +0300

    Add recipeReducer

--- src/action/index.js
+++ src/action/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const FILTER_RECIPE = 'FILTER_RECIPE';
-const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST';
-const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
-const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
+export const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST';
+export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS';
+export const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE';
 export const recipeFilter = (filter) => ({
   type: FILTER_RECIPE,
   filter,
