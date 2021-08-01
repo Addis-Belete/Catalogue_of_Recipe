9f356d89356ff79741d8a5dfb54417ef0efaf3cb
Addis Belete <addis.bel@gmail.com>
2021-08-01 19:01:28 +0300

    Add mapDispatchToProps method

--- src/container/ItemList.js
+++ src/container/ItemList.js
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Recipe from '../components/Recipe';
+import { fetchRecipes } from '../action';
 
 const ItemList = ({ recipes }) => (
   <Recipe recipes={recipes} />
@@ -9,6 +10,10 @@ const ItemList = ({ recipes }) => (
 );
 const mapStateToProps = (state) => ({
   recipes: state.results,
+
+});
+const mapDispatchToProps = (dispatch) => ({
+  fetchRecipes: () => dispatch(fetchRecipes()),
 
 });
 
