20748af3dfae5b94c970ce01089a1ec005df50ff
Addis Belete <addis.bel@gmail.com>
2021-08-02 16:43:15 +0300

    Add intialState to ingredientRedcer

---
+++ src/reducer/ingredientReducer.js
@@ -0,0 +1,12 @@
+import {
+  FETCH_INGREDIENT_FAILURE, FETCH_INGREDIENT_REQUEST, FETCH_INGREDIENT_SUCCESS, fetchIngredients,
+} from '../action/ingredient';
+
+initialState = {
+  ingredients: [{
+    name: 'bocola',
+    image: '',
+
+  }],
+
+};
