5c2b2370ef72f160d2bbffa59ad796e318ee75b5
Addis Belete <addis.bel@gmail.com>
2021-08-02 09:17:46 +0300

    Add filterReducer to rootReducer

--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import recipeReducer from './recipeReducer';
+import filterReducer from './filter';
 
 const rootReducer = combineReducers({
   recipe: recipeReducer,
-
+  filter: filterReducer,
 });
 
 export default rootReducer;
