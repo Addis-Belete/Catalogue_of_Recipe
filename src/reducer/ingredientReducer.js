d3179d5e63d5df24e1bd733e4389fa21e7b2a231
Addis Belete <addis.bel@gmail.com>
2021-08-02 17:39:21 +0300

    Add ingredients reducer

--- src/reducer/ingredientReducer.js
+++ src/reducer/ingredientReducer.js
@@ -2,11 +2,40 @@ import {
   FETCH_INGREDIENT_FAILURE, FETCH_INGREDIENT_REQUEST, FETCH_INGREDIENT_SUCCESS, fetchIngredients,
 } from '../action/ingredient';
 
-initialState = {
+const initialState = {
+  loading: false,
   ingredients: [{
     name: 'bocola',
     image: '',
 
   }],
-
+  error: '',
 };
+
+const ingredientReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_INGREDIENT_REQUEST:
+      return {
+        loading: true,
+        ingredients: [],
+        error: '',
+      };
+    case FETCH_INGREDIENT_SUCCESS:
+      return {
+        loading: false,
+        ingredients: action.payload,
+        error: '',
+
+      };
+    case FETCH_INGREDIENT_FAILURE:
+      return {
+        loading: false,
+        ingredients: [],
+        error: action.payload,
+
+      };
+    default:
+      return state;
+  }
+};
+export default ingredientReducer;
