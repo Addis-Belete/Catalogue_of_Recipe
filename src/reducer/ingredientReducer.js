79267cc8e8375c5d3e1b480cb76daa52ae9682f7
Addis Belete <addis.bel@gmail.com>
2021-08-02 17:41:55 +0300

    ADD ingredientReducer to rootReducer

--- src/reducer/ingredientReducer.js
+++ src/reducer/ingredientReducer.js
@@ -1,6 +1,4 @@
-import {
-  FETCH_INGREDIENT_FAILURE, FETCH_INGREDIENT_REQUEST, FETCH_INGREDIENT_SUCCESS, fetchIngredients,
-} from '../action/ingredient';
+import { FETCH_INGREDIENT_FAILURE, FETCH_INGREDIENT_REQUEST, FETCH_INGREDIENT_SUCCESS } from '../action/ingredient';
 
 const initialState = {
   loading: false,
--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import recipeReducer from './recipeReducer';
+import ingredientReducer from './ingredientReducer';
 
 const rootReducer = combineReducers({
   recipe: recipeReducer,
-
+  ingredient: ingredientReducer,
 });
 
 export default rootReducer;
