7ab0f143b285b17280c0399a834d2a6d16ec36be
Addis Belete <addis.bel@gmail.com>
2021-08-04 12:30:50 +0300

    update api key

--- src/action/ingredient.js
+++ src/action/ingredient.js
@@ -20,9 +20,8 @@ export const fetchIngredientsFailure = (error) => ({
 
 });
 export const fetchIngredients = (id) => (dispatch) => {
-  const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
   dispatch(fetchIngredientRequests);
-  axios.get(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${API_KEY}`)
+  axios.get(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=091d7eadd4f54a608319bdf48fd10453`)
     .then((response) => {
       const ingredients = response.data;
       dispatch(fetchIngredientSuccess(ingredients));
