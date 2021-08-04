1b1b2da086a0c9d1faa28bda5497524d19d9d77c
Addis Belete <addis.bel@gmail.com>
2021-08-04 12:21:43 +0300

    Update api key

--- src/action/index.js
+++ src/action/index.js
@@ -24,7 +24,7 @@ export const fetchRecipesFailure = (error) => ({
 export const fetchRecipes = () => (dispatch) => {
   const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
   dispatch(fetchRecipeRequests);
-  axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=b79fd0809dbc4049bcf789f44f40c7f2&number=12')
+  axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=12`)
     .then((response) => {
       const recipes = response.data;
       dispatch(fetchRecipeSuccess(recipes));
--- src/action/ingredient.js
+++ src/action/ingredient.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import axios from 'axios';
 
 export const FETCH_INGREDIENT_REQUEST = 'FETCH_USER_REQUEST';
@@ -21,8 +20,9 @@ export const fetchIngredientsFailure = (error) => ({
 
 });
 export const fetchIngredients = (id) => (dispatch) => {
+  const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
   dispatch(fetchIngredientRequests);
-  axios.get(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=b79fd0809dbc4049bcf789f44f40c7f2`)
+  axios.get(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${API_KEY}`)
     .then((response) => {
       const ingredients = response.data;
       dispatch(fetchIngredientSuccess(ingredients));
