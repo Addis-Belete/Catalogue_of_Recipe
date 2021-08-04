72d5e882e5fd6e0c3fa9860ce5eed3b7589fa3e1
Addis Belete <addis.bel@gmail.com>
2021-08-04 09:39:10 +0300

    Add test to Ingredients component

---
+++ src/__test__/component/Ingredients.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Ingredients from '../../container/Ingeredients';
+import store from '../../store/store';
+
+afterEach(cleanup);
+it('displays Ingredients', () => {
+  const recipe = [{ title: 'add', id: 123, image: '.jepg' }];
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Ingredients recipes={recipe} />
+      </BrowserRouter>
+    </Provider>,
+  );
+  expect(screen.getByText('Ingredients')).toBeTruthy();
+});
